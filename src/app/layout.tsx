2021ce8359b9f07858570782b4e10e62ddb976ea
xlzbthxyjkoo <happyyj01@naver.com>
2025-04-04 16:24:40 +0900

    style: favicon, font setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,64 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata, Viewport } from "next";
+import { Nanum_Gothic, Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+// 한글 폰트 - 나눔고딕
+const nanumGothic = Nanum_Gothic({
+  weight: ["400", "700", "800"],
   subsets: ["latin"],
+  variable: "--font-nanum-gothic",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// 영어 폰트 - Roboto
+const roboto = Roboto({
+  weight: ["300", "400", "700"],
+  style: ["normal", "italic"],
   subsets: ["latin"],
+  variable: "--font-roboto",
 });
+
+// viewport 설정 추가
+export const viewport: Viewport = {
+  themeColor: "#ffffff",
+};
 
 export const metadata: Metadata = {
   title: "Mu:it",
   description: "앨범을 담다.",
   icons: {
-    icon: "/favicon.png",
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+    ],
+    shortcut: [{ url: "/favicon.ico" }],
+    apple: [
+      { url: "/apple-icon-57x57.png", sizes: "57x57", type: "image/png" },
+      { url: "/apple-icon-60x60.png", sizes: "60x60", type: "image/png" },
+      { url: "/apple-icon-72x72.png", sizes: "72x72", type: "image/png" },
+      { url: "/apple-icon-76x76.png", sizes: "76x76", type: "image/png" },
+      { url: "/apple-icon-114x114.png", sizes: "114x114", type: "image/png" },
+      { url: "/apple-icon-120x120.png", sizes: "120x120", type: "image/png" },
+      { url: "/apple-icon-144x144.png", sizes: "144x144", type: "image/png" },
+      { url: "/apple-icon-152x152.png", sizes: "152x152", type: "image/png" },
+      { url: "/apple-icon-180x180.png", sizes: "180x180", type: "image/png" },
+    ],
+    other: [
+      {
+        rel: "icon",
+        url: "/android-icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "manifest",
+        url: "/manifest.json",
+      },
+    ],
+  },
+  other: {
+    "msapplication-TileColor": "#ffffff",
+    "msapplication-TileImage": "/ms-icon-144x144.png",
   },
 };
 
@@ -28,7 +70,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nanumGothic.variable} ${roboto.variable} antialiased`}
       >
         {children}
       </body>
